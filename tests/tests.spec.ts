d10478233bdf91d34c56c51149d66424c8f1cdd7
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2026-07-21 21:41:17 +0200

    Expand e2e test

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -7,14 +7,14 @@ test("should be able to execute the first test of the example project", async ({
   await expect(workbox).toHaveTitle(
     "[Extension Development Host] fixture - VSCodium",
   );
-  // await workbox.getByRole("button", { name: "Reopen in Container" }).click();
+  await workbox.getByRole("button", { name: "Reopen in Container" }).click();
 
-  // await expect(workbox).toHaveTitle(
-  //   "[Extension Development Host] fixture [Dev Container] - VSCodium",
-  //   { timeout: 120_000 },
-  // );
-  // await workbox.getByRole("button", { name: "remote  Dev Container" }).click();
-  // await workbox
-  //   .getByRole("option", { name: "Close Remote Connection" })
-  //   .click();
+  await expect(workbox).toHaveTitle(
+    "[Extension Development Host] fixture [Dev Container] - VSCodium",
+    { timeout: 120_000 },
+  );
+  await workbox.getByRole("button", { name: "remote  Dev Container" }).click();
+  await workbox
+    .getByRole("option", { name: "Close Remote Connection" })
+    .click();
 });
