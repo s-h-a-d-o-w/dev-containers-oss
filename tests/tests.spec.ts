6de50368abd14289f646958fb019bd44fefc5080
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2026-07-21 21:34:12 +0200

    Try on ci

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -3,17 +3,18 @@ import { test, expect } from "./baseTest";
 test("should be able to execute the first test of the example project", async ({
   workbox,
 }) => {
+  // await workbox.pause();
   await expect(workbox).toHaveTitle(
     "[Extension Development Host] fixture - VSCodium",
   );
-  await workbox.getByRole("button", { name: "Reopen in Container" }).click();
+  // await workbox.getByRole("button", { name: "Reopen in Container" }).click();
 
-  await expect(workbox).toHaveTitle(
-    "[Extension Development Host] fixture [Dev Container] - VSCodium",
-    { timeout: 120_000 },
-  );
-  await workbox.getByRole("button", { name: "remote  Dev Container" }).click();
-  await workbox
-    .getByRole("option", { name: "Close Remote Connection" })
-    .click();
+  // await expect(workbox).toHaveTitle(
+  //   "[Extension Development Host] fixture [Dev Container] - VSCodium",
+  //   { timeout: 120_000 },
+  // );
+  // await workbox.getByRole("button", { name: "remote  Dev Container" }).click();
+  // await workbox
+  //   .getByRole("option", { name: "Close Remote Connection" })
+  //   .click();
 });
